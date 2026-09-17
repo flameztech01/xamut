62dd1ffedcaed23bc87f1e0e762b7c3e1bf67e89
flameztech01 <flameztech01@gmail.com>
2026-09-17 16:56:17 +0100

    new

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,7 @@ app.use(cookieParser());
 app.use(cors({
     origin: [
         "http://localhost:3000",
+        "https://xamut.curriumx.online",
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH','OPTIONS'],
